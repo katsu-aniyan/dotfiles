ad5b61f50b92e9b25b64a82e6b2021cccc006337
Katsushige Aniya <katsu.aniyan@gmail.com>
2017-04-08 20:37:33 +0900

    add .git files

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
