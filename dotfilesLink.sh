82c1e8751acacaabe1ce8a4355ab2724c21fe68a
Katsushige Aniya <katsu.aniyan@gmail.com>
2017-08-11 18:22:28 +0900

    add .tigrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
