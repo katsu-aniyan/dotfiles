b993fc9c40eac32c9f9ffb3d04b1a15013ce3689
Katsushige Aniya <katsu.aniyan@gmail.com>
2017-08-12 20:26:26 +0900

    add tmux.conf

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
