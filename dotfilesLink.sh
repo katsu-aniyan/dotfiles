30c7893752316ec38f6eed2191190b90d5876854
Katsushige Aniya <katsu.aniyan@gmail.com>
2017-10-04 09:32:55 +0900

    add monokai scheme memo

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,8 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# xfce4 monokai scheme
+# git clone https://gist.github.com/tsbarnes/d9dca461edab08b98b0e
+# place in 
+# ~/.local/share/xfce4/terminal/colorschemes/
